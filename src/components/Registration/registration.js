f5c35ed66458c2f6b554f1eb90df8b98f5bded9f
torosyan1 <torosyan252@gmail.com>
2020-07-15 00:34:56 +0400

    registration_data_push_DB

--- src/components/Registration/registration.js
+++ src/components/Registration/registration.js
@@ -1,5 +1,85 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
+import CheckCircleIcon from "@material-ui/icons/CheckCircle";
+import { useHistory } from "react-router-dom";
+
+export const Registration = () => {
+  const history = useHistory();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [backendData, setBackendData] = useState("");
+
+  const handelSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post(`${process.env.REACT_APP_IP}/registration`, {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: password,
+      })
+      .then((res) => setBackendData(res.data.msg))
+      .catch((error) => console.log(error));
+    console.log(process.env.REACT_APP_IP);
+  };
+
+  if (backendData === "Successfull") {
+    setTimeout(() => {
+      history.push("/");
+    }, 2000);
+  }
+
+  return (
+    <>
+      <Contanier>
+        {backendData === "Successfull" ? (
+          <Overlay>
+            <SuccessAlert>
+              <CheckCircleIcon style={{ color: "green" }} />
+              {backendData}
+            </SuccessAlert>
+          </Overlay>
+        ) : (
+          <Form onSubmit={handelSubmit}>
+            <RegistrationParagraf>Registration</RegistrationParagraf>
+            <BackendDataMsg>{backendData}</BackendDataMsg>
+            <Input
+              value={firstName}
+              type="text"
+              placeholder="First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
+            <Input
+              value={lastName}
+              type="text"
+              placeholder="Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+            />
+            <Input
+              value={email}
+              type="text"
+              placeholder="Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              value={password}
+              type="password"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button>Submit</Button>
+            <GoToSignIn href="/">Sign In</GoToSignIn>
+          </Form>
+        )}
+      </Contanier>
+    </>
+  );
+};
 
 const Contanier = styled.div`
   display: flex;
@@ -36,6 +116,7 @@ const Form = styled.form`
   box-shadow: 23.8596px 5.61404px 50px rgba(0, 0, 0, 0.02),
     35.0877px 0px 70px rgba(86, 128, 248, 0.05),
     14.0351px 0px 25px rgba(86, 128, 248, 0.03);
+  z-index: 0;
 `;
 
 const Input = styled.input`
@@ -67,46 +148,48 @@ const Button = styled.button`
     background: linear-gradient(100.61deg, #7cb8f7 100%, #2a8bf2 0%);
   }
 `;
+const BackendDataMsg = styled.p`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: auto;
+  color: red;
+  background: none;
+  padding: 10px;
+`;
 
-export const Registration = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+const Overlay = styled.div`
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.4);
+  z-index: 20;
+  cursor: pointer;
+`;
 
-  return (
-    <>
-      <Contanier>
-        <Form>
-          <RegistrationParagraf>Registration</RegistrationParagraf>
-          <Input
-            value={firstName}
-            type="text"
-            placeholder="First Name"
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <Input
-            value={lastName}
-            type="text"
-            placeholder="Last Name"
-            onChange={(e) => setLastName(e.target.value)}
-          />
-          <Input
-            value={email}
-            type="text"
-            placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
-          />
+const SuccessAlert = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  width: 30%;
+  height: 30%;
+  border-radius: 10px;
+  padding: 10px 10px;
+`;
 
-          <Input
-            value={password}
-            type="password"
-            placeholder="Password"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button>Submit</Button>
-        </Form>
-      </Contanier>
-    </>
-  );
-};
+const GoToSignIn = styled.a`
+  display: flex;
+  justify-content: flex-end;
+  text-decoration: none;
+  color: #2a8bf2;
+  text-decoration: none;
+  background: none;
+  padding-top: 20px;
+`;
