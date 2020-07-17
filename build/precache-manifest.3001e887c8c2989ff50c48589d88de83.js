803a15ca8d3780f4c53d5ac4ea090e67b58e1ec2
torosyan1 <torosyan252@gmail.com>
2020-07-17 04:11:48 +0400

    Deploy

---
+++ build/precache-manifest.3001e887c8c2989ff50c48589d88de83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6909cb6bf82122f67a3dd5d701d7353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ddf0ed492d833ff1c90",
+    "url": "/static/css/main.1478dd27.chunk.css"
+  },
+  {
+    "revision": "508d4130d00a2e5f6e87",
+    "url": "/static/js/2.eac7d7bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eac7d7bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ddf0ed492d833ff1c90",
+    "url": "/static/js/main.b43aef85.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "38aced5b5d46fc7d64a8412bc07cdde2",
+    "url": "/static/media/TTNorms-Medium.38aced5b.otf"
+  }
+]);
